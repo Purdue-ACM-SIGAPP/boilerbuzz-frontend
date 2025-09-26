1d6f81c10c748273d8c962419c9d447e17dc1831
liudan4 <liu4228@purdue.edu>
2025-09-25 22:05:44 -0400

    Designed the Add Event Page UI

--- src/navigation/RootStackNavigator.tsx
+++ src/navigation/RootStackNavigator.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import BottomTabsNavigator from "./BottomTabsNavigator";
 import type { RootStackParamList } from "./types";
+import AddEventPage from "../screens/AddEventPage";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -19,6 +20,7 @@ export default function RootStackNavigator() {
         <Stack.Screen name="Tabs" component={BottomTabsNavigator} />
         {/* Add other screens here as needed, e.g.: */}
         {/* <Stack.Screen name="Details" component={DetailsScreen} /> */}
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
