d51d2041631d886006c3fc052afb3b77367065b8
Colin Wu <colinwu0403@gmail.com>
2025-09-30 13:38:01 -0400

    Login Page as initial screen

--- src/screens/LoginPage.tsx
+++ src/screens/LoginPage.tsx
@@ -1,21 +1,26 @@
 // src/screens/LoginPage.tsx
 
 //Remember to also transfer from login page to questions about demographics and clubs
-import React, {useState} from "react";
-import {ImageBackground, View, Text, StyleSheet, TextInput, Alert} from "react-native";
-import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
-import MyButton from "../components/button";
-import { createStaticNavigation } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useState } from "react";
+import {
+  ImageBackground,
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Alert,
+} from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import MyButton from "../components/MyButton";
+import { createStaticNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useNavigation } from "@react-navigation/native";
 
-
-
 export default function LoginPage() {
-    const navigation = useNavigation();
-    const [username, setUsername] = React.useState('');
-    const [password, setPassword] = React.useState('');
-    return (
+  const navigation = useNavigation();
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
         <TextInput
@@ -31,22 +36,23 @@ export default function LoginPage() {
           keyboardType="default"
           style={styles.textbox}
         />
-        <MyButton 
-        title="Login" 
-        onPress={() => {
-          if (!username || !password) {
-            Alert.alert('Error', 'Please enter both username and password.');
-          }
-          else {
-            navigation.navigate('Tabs');
-          }
-        }
-        } />
-        
         <MyButton
-        title="Register" 
-        onPress={() => {
-        navigation.navigate('Tabs'); }} />
+          title="Login"
+          onPress={() => {
+            if (!username || !password) {
+              Alert.alert("Error", "Please enter both username and password.");
+            } else {
+              navigation.navigate("Tabs");
+            }
+          }}
+        />
+
+        <MyButton
+          title="Register"
+          onPress={() => {
+            navigation.navigate("Tabs");
+          }}
+        />
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -59,15 +65,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  textbox: { 
+  textbox: {
     margin: 10,
     height: 30,
     fontSize: 16,
     width: 300,
     borderColor: "black",
     borderWidth: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffffffff',
-},
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffffff",
+  },
 });
