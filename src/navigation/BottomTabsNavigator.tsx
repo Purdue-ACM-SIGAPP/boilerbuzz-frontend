901ff09775dceeac1a9cb909c371ccdcefef291d
Colin Wu <colinwu0403@gmail.com>
2025-10-02 20:33:43 -0400

    Profile Page

--- src/navigation/BottomTabsNavigator.tsx
+++ src/navigation/BottomTabsNavigator.tsx
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import FeedPage from "../screens/FeedPage";
 import FeaturedPage from "../screens/FeaturedPage";
 import BoardPage from "../screens/BoardPage";
+import ProfilePage from "../screens/ProfilePage";
 
 import type { BottomTabsParamList } from "./types";
 
@@ -10,10 +11,14 @@ const Tab = createBottomTabNavigator<BottomTabsParamList>();
 
 export default function BottomTabsNavigator() {
   return (
-    <Tab.Navigator initialRouteName="Feed" screenOptions={{ headerShown: false }}>
+    <Tab.Navigator
+      initialRouteName="Feed"
+      screenOptions={{ headerShown: false }}
+    >
       <Tab.Screen name="Feed" component={FeedPage} />
       <Tab.Screen name="Featured" component={FeaturedPage} />
       <Tab.Screen name="Board" component={BoardPage} />
+      <Tab.Screen name="Profile" component={ProfilePage} />
     </Tab.Navigator>
   );
 }
