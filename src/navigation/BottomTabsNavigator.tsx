5b3d1d472b97b1cd7a153e17b12a348f5815ec05
SashimiDaBest <soleilpham@gmail.com>
2025-10-02 20:55:31 -0400

    revise navigation error

--- src/navigation/BottomTabsNavigator.tsx
+++ src/navigation/BottomTabsNavigator.tsx
@@ -3,6 +3,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import FeedPage from "../screens/FeedPage";
 import FeaturedPage from "../screens/FeaturedPage";
 import BoardPage from "../screens/BoardPage";
+import SearchPage from "../screens/SearchPage";
+import ProfilePage from "../screens/ProfilePage";
 
 import type { BottomTabsParamList } from "./types";
 
@@ -10,10 +12,12 @@ const Tab = createBottomTabNavigator<BottomTabsParamList>();
 
 export default function BottomTabsNavigator() {
   return (
-    <Tab.Navigator initialRouteName="Feed" screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Feed" component={FeedPage} />
-      <Tab.Screen name="Featured" component={FeaturedPage} />
-      <Tab.Screen name="Board" component={BoardPage} />
+    <Tab.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="Home" component={FeedPage} />
+      <Tab.Screen name="Trending" component={FeaturedPage} />
+      <Tab.Screen name="Search" component={SearchPage} />
+      <Tab.Screen name="Pinned" component={BoardPage} />
+      <Tab.Screen name="Profile" component={ProfilePage} />
     </Tab.Navigator>
   );
 }
--- src/screens/FeaturedPage.tsx
+++ src/screens/FeaturedPage.tsx
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from "react-native";
 import type { BottomTabsParamList } from "../navigation/types";
 import type { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 
-type Props = BottomTabScreenProps<BottomTabsParamList, "Feed">;
+type Props = BottomTabScreenProps<BottomTabsParamList, "Trending">;
 
 export default function FeaturedPage({ navigation, route }: Props) {
   return (
