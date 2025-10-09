c78498d16d1a04b1c40c390b02f8866fea71208c
JustSomeone1223 <73112679+JustSomeone1223@users.noreply.github.com>
2025-10-09 22:36:05 +0000

    This should be the right icons.

--- src/navigation/BottomTabsNavigator.tsx
+++ src/navigation/BottomTabsNavigator.tsx
@@ -5,6 +5,7 @@ import TrendingPage from "../screens/TrendingPage";
 import EventsPage from "../screens/EventsPage";
 import PinnedPage from "../screens/PinnedPage";
 import ProfilePage from "../screens/ProfilePage";
+import { Ionicons } from '@expo/vector-icons';
 
 import type { BottomTabsParamList } from "./types";
 
@@ -12,7 +13,32 @@ const Tab = createBottomTabNavigator<BottomTabsParamList>();
 
 export default function BottomTabsNavigator() {
   return (
-    <Tab.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+    <Tab.Navigator initialRouteName="Home"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName: string;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Trending') {
+            iconName = focused ? 'podium' : 'podium-outline';
+          } else if (route.name === 'Events') {
+            iconName = focused ? 'search' : 'search-outline';
+          } else if (route.name === 'Pinned') {
+            iconName = focused ? 'pin' : 'pin-outline';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'person' : 'person-outline';
+          } else {
+            iconName = focused ? 'add' : 'add-outline';
+          }
+
+          return <Ionicons name={iconName as any} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+        headerShown: false
+      })}
+    >
       <Tab.Screen name="Home" component={HomePage} />
       <Tab.Screen name="Trending" component={TrendingPage} />
       <Tab.Screen name="Events" component={EventsPage} />
