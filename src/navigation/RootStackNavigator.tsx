b209beb8c053bb3409191b399fe86c56860e5b9f
gx1221 <georgex770@gmail.com>
2025-10-08 23:09:24 -0400

    Update navigator to include RegisterPage

--- src/navigation/RootStackNavigator.tsx
+++ src/navigation/RootStackNavigator.tsx
@@ -4,8 +4,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import BottomTabsNavigator from "./BottomTabsNavigator";
 import LoginPage from "../screens/LoginPage";
+import RegisterPage from "../screens/RegisterPage"
 import type { RootStackParamList } from "./types";
-
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function RootStackNavigator() {
@@ -19,6 +19,7 @@ export default function RootStackNavigator() {
       >
         <Stack.Screen name="Tabs" component={BottomTabsNavigator} />
         <Stack.Screen name="Login" component={LoginPage} />
+        <Stack.Screen name ="Register" component={RegisterPage} />
         {/* Add other screens here as needed, e.g.: */}
         {/* <Stack.Screen name="Details" component={DetailsScreen} /> */}
       </Stack.Navigator>
