cc84d301938d431c3afb7182e64ce3e504c83c45
SashimiDaBest <soleilpham@gmail.com>
2025-10-09 20:00:27 -0400

    current progress

--- src/navigation/BottomTabsNavigator.tsx
+++ src/navigation/BottomTabsNavigator.tsx
@@ -5,15 +5,38 @@ import FeaturedPage from "../screens/FeaturedPage";
 import BoardPage from "../screens/BoardPage";
 import SearchPage from "../screens/SearchPage";
 import ProfilePage from "../screens/ProfilePage";
-
+import { Image } from "react-native";
 import type { BottomTabsParamList } from "./types";
 
 const Tab = createBottomTabNavigator<BottomTabsParamList>();
 
 export default function BottomTabsNavigator() {
   return (
-    <Tab.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Home" component={FeedPage} />
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{ headerShown: false }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={FeedPage}
+        options={{
+          tabBarLabel: "Home",
+          // tabBarIcon: ({ focused }) => (
+            // <Image
+            //   source={
+            //     focused
+                  // ? require("../../../assets/icons/home_pressed.png")
+                  // : require("../../../assets/icons/home.png")
+            //   }
+            //   style={{
+            //     width: 24,
+            //     height: 24,
+            //     tintColor: focused ? "#007AFF" : "#8e8e93", // optional tint
+            //   }}
+            // />
+          // ),
+        }}
+      />
       <Tab.Screen name="Trending" component={FeaturedPage} />
       <Tab.Screen name="Search" component={SearchPage} />
       <Tab.Screen name="Pinned" component={BoardPage} />
