d273ddbd17eaa57608d193c46432f7bb7b69e860
JoshPotatoesRaven <Joshuacma12@Gmail.com>
2025-11-06 20:37:13 -0500

    adjusted card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,8 @@
-import picture from 'profile.png';
+import picture from './assets/profile.png';
 function Card() {
     return (
         <div> 
-            <img src={picture} at = "Card Image" />
+            <img src={picture} alt = "Card Image" />
             <h2> Events Details </h2>
         </div>
     )
